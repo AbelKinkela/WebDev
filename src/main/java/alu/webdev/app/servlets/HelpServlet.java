7ca229ee596c1dc027f5a26b533e348deb6abb22
AbelKinkela <akinkela16@alustudent.com>
2019-12-07 17:01:18 +0400

    commit 1--A working template of our project with Java servlet and Maven

---
+++ src/main/java/alu/webdev/app/servlets/HelpServlet.java
@@ -0,0 +1,17 @@
+package alu.webdev.app.servlets;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelpServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("HELP PAGE");
+    }
+}
---
+++ src/main/java/alu/webdev/app/servlets/HomeServlet.java
@@ -0,0 +1,21 @@
+package alu.webdev.app.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HomeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.html");
+        requestDispatcher.forward(request, response);
+    }
+}
