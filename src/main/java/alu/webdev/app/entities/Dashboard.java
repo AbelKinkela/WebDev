b1275ad317dd001abd9cf525466e2faf38d28c10
AbelKinkela <akinkela16@alustudent.com>
2019-12-09 00:11:08 +0400

    Commit 1 - Added code in the Dashboard.java for the logic of my Dashboard page.

--- src/main/java/alu/webdev/app/entities/Dashboard.java
+++ src/main/java/alu/webdev/app/entities/Dashboard.java
@@ -1,4 +1,39 @@
 package alu.webdev.app.entities;
 
+import java.util.ArrayList;
+
+/**
+ *
+ * @author AbelKinkela
+ */
 public class Dashboard {
+
+    private ArrayList<Project> projects;
+
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<Project> projects) {
+        this.projects = projects;
+    }
+
+
+
+    /**
+     *
+     */
+    public void createProject(){
+        projects = new ArrayList<Project>();
+        Project project = new Project();
+        projects.add(project);
+    }
+
+    public void deleteProject(Project project){
+        projects.remove(project);
+    }
+
 }
+
+
+
