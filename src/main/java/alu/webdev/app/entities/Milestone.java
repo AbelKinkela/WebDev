15c93af0c084071bdb0e46379b2bd734e46b74dd
Sumaya Salihs <myayasaleehs@gmail.com>
2019-12-14 00:35:07 +0400

    updates the Milestone class

--- src/main/java/alu/webdev/app/entities/Milestone.java
+++ src/main/java/alu/webdev/app/entities/Milestone.java
@@ -2,13 +2,10 @@ package alu.webdev.app.entities;
 
 public class Milestone {
     private String name;
-    private Status status;
 
 
     public Milestone(String name) {
         this.name = name;
-        Status status = new Status();
-        this.status = status;
     }
 
 
@@ -21,25 +18,6 @@ public class Milestone {
         this.name = name;
     }
 
-
-    public Status getStatus() {
-        return status;
-    }
-
-
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Milestone{" +
-                "name='" + name + '\'' +
-                ", status=" + status +
-                '}';
-    }
 
 
 }
