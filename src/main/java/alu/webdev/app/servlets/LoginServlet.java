9e10661804d1eb042c336f3501856032607b7b24
aragabb <56732501+aragabb@users.noreply.github.com>
2019-12-15 16:42:06 +0400

    modifed login servlet and css

--- src/main/java/alu/webdev/app/servlets/LoginServlet.java
+++ src/main/java/alu/webdev/app/servlets/LoginServlet.java
@@ -1,4 +1,41 @@
 package alu.webdev.app.servlets;
 
-public class LoginServlet {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet{
+    public LoginServlet() {
+        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        if(email.isEmpty() || password.isEmpty() )
+        {
+            RequestDispatcher req = request.getRequestDispatcher("login.jsp");
+            req.include(request, response);
+        }
+        else
+        {
+            RequestDispatcher req = request.getRequestDispatcher("dashboard.jsp");
+            req.forward(request, response);
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
