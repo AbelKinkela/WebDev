ecebc25f4b11ca59fc533f018cad7bddf2fbe96c
Sumaya Salihs <myayasaleehs@gmail.com>
2019-12-15 22:03:15 +0400

    add config files

---
+++ src/main/java/alu/webdev/app/dao/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package alu.webdev.app.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// This class can be used to initialize the database connection
+public class DatabaseConnection {
+    public static Connection initializeDatabase()
+            throws SQLException, ClassNotFoundException
+    {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String dbURL = "jdbc:mysql://localhost:3306/WebDev";
+        String dbUsername = "root";
+        String dbPassword = "2011butterfly";
+        Connection con = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+        return  con;
+
+
+    }
+}
